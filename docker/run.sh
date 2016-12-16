53515e8cf8864cb3bebc61b31e948861c94de37e
koolay <koolay@163.com>
2016-12-16 14:09:14 +0800

    flower

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-celery -A proj flower --port=5555 & celery -A $APP_NAME worker --loglevel=$LOG_LEVEL --concurrency=$CONCURRENCY -n worker@%h
+celery -A $APP_NAME flower --port=5555 & celery -A $APP_NAME worker --loglevel=$LOG_LEVEL --concurrency=$CONCURRENCY -n worker@%h
